c7d4e364ebe5eb0bbf83082631c74fca5845d317
Kaluã Bentes <kalua.bentes@hotmart.com>
2022-05-20 16:28:12 -0400

    Create navbar

--- src/components/core/NavBar.vue
+++ src/components/core/NavBar.vue
@@ -1,18 +1,106 @@
 <template>
   <header class="navbar">
-    <img class="navbar__logo" src="@/assets/logo.svg" alt="" />
+    <div class="navbar__brand-group">
+      <img class="navbar__logo" src="@/assets/logo.svg" alt="" />
+      <button class="navbar__organization">
+        <h2 class="navbar__organization-title">Surreal São Paulo</h2>
+        <p class="navbar__organization-subtitle">VTEX</p>
+      </button>
+    </div>
+    <div class="navbar__profile-group">
+      <button class="navbar__notification-button">
+        <img
+          class="navbar__notification-icon"
+          src="@/assets/icone-bell.svg"
+          alt=""
+        />
+      </button>
+      <div class="navbar__separator"></div>
+      <p class="navbar__profile-greetings">Olá, Joaquim</p>
+      <button class="navbar__profile-button">
+        <img src="@/assets/usuario.svg" alt="" />
+      </button>
+    </div>
   </header>
 </template>
 
 <style scoped lang="scss">
 .navbar {
   background: var(--color-primary);
+  color: var(--color-white);
   width: 100%;
   height: 5rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 1.5rem;
+
+  &__brand-group {
+    display: flex;
+    align-items: center;
+    gap: 1.5rem;
+  }
 
   &__logo {
     height: 3.125rem;
     width: 3.125rem;
+  }
+
+  &__organization {
+    height: 3.75rem;
+    width: 17rem;
+    border: 2px solid var(--color-white);
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 0 1rem;
+    background: transparent;
+    color: var(--color-white);
+    font-family: var(--font-family);
+    cursor: pointer;
+
+    &-title {
+      font-size: 1rem;
+    }
+
+    &-subtitle {
+      font-size: 0.75rem;
+      font-weight: 400;
+    }
+  }
+
+  &__profile-group {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  &__notification-button {
+    background: transparent;
+    border: none;
+    outline: none;
+    padding: 1rem;
+    cursor: pointer;
+  }
+
+  &__separator {
+    height: 5rem;
+    width: 2px;
+    background: var(--color-white);
+    opacity: 0.2;
+  }
+
+  &__profile-greetings {
+    padding: 1rem;
+    font-weight: 700;
+    font-size: 1rem;
+  }
+
+  &__profile-button {
+    background: transparent;
+    border: none;
+    outline: none;
   }
 }
 </style>
--- src/components/layouts/AppLayout.vue
+++ src/components/layouts/AppLayout.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import NavBar from "../core/NavBar.vue";
+import NavBar from "@/components/core/NavBar.vue";
 </script>
 
 <template>
